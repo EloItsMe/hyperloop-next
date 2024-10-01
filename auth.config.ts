49ffb0e0d7771f99be32c8790d4c619307abe910
Elowan Audouin <elowan.aud@proton.me>
2024-10-01 20:22:39 +0200

    replace drizzle to prisma

--- auth.config.ts
+++ auth.config.ts
@@ -1,15 +1,56 @@
-import { db } from "@/db";
 import { env } from "@/env";
-import { DrizzleAdapter } from "@auth/drizzle-adapter";
-import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { User } from "@prisma/client";
+import NextAuth, { DefaultSession } from "next-auth";
 import GitHub from "next-auth/providers/github";
+import { prisma } from "./prisma";
+
+declare module "next-auth" {
+  interface Session {
+    user: User & DefaultSession["user"];
+  }
+}
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: DrizzleAdapter(db),
+  adapter: PrismaAdapter(prisma),
   providers: [
     GitHub({
       clientId: env.GITHUB_ID,
       clientSecret: env.GITHUB_SECRET,
     }),
   ],
+  callbacks: {
+    async session({ session }) {
+      if (!session.user) {
+        return session;
+      }
+
+      const user = await prisma.user.findUnique({
+        where: {
+          email: session.user.email,
+        },
+      });
+
+      // @ts-expect-error Auth.js typings are wrong
+      session.user = user;
+
+      return session;
+    },
+  },
+  events: {
+    async linkAccount({ user }) {
+      if (!user.email) {
+        return;
+      }
+
+      await prisma.user.update({
+        where: {
+          email: user.email,
+        },
+        data: {
+          emailVerified: new Date(),
+        },
+      });
+    },
+  },
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { getCurrentUser } from "@/lib/utils";
 
 export default async function HomePage() {
   const currentUser = await getCurrentUser();
-  console.log(currentUser);
+
   return (
     <>
       <h1>Hello world</h1>
